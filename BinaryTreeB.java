24462313721261df10ef94cb4f2e7ff00d07df9f
Harshitrop <harshitrathourop@gmail.com>
2026-04-06 17:46:17 +0530

    update BTree

--- BinaryTreeB.java
+++ BinaryTreeB.java
@@ -227,19 +227,166 @@ public class BinaryTreeB {
         }
         System.out.println();
     }
+
+    //-----Kth level of tree problem------
+    public static void KthLevel(node root, int Level, int k){
+        if(root == null){
+            return;
+        }
+        if(k == Level){
+            System.out.print(root.data + " ");
+            return;
+        }
+        KthLevel(root.left, Level+1, k);
+        KthLevel(root.right, Level+1, k);
+    }
+
+
+    //----Lowest Common Ancestor of Binary Tree problem------
+    public static boolean Getpath(node root, int n, ArrayList<node> path){
+        if(root == null){
+            return false;
+        }
+        path.add(root);
+        if(root.data == n){
+            return true;
+        }
+        if(Getpath(root.left, n, path) || Getpath(root.right, n, path)){
+            return true;
+        }
+        path.remove(path.size() - 1);
+        return false;
+    }
+    public static node LCA(node root, int n1, int n2){//O(n)
+        ArrayList<node> path1 = new ArrayList<>();
+        ArrayList<node> path2 = new ArrayList<>();
+
+        Getpath(root, n1, path1);
+        Getpath(root, n2, path2);
+
+        //last common ancestor
+        int i =0;
+        for(; i < path1.size() && i < path2.size(); i++){
+            if(path1.get(i) != path2.get(i)){
+                break;
+            }
+        }
+        //last equal node -> ith -1
+        node lca = path1.get(i-1);
+        return lca;
+    }
+    //Optimized LCA approach
+    public static node LCA2(node root, int n1, int n2){
+        if(root == null || root.data == n1 || root.data == n2){
+            return root;
+        }
+
+        node leftLCA = LCA2(root.left, n1, n2);
+        node rightLCA = LCA2(root.right, n1, n2);
+
+        if(rightLCA == null){
+            return leftLCA;
+        }
+        if(leftLCA == null){
+            return rightLCA;
+        }
+        return root;
+    }
+
+    //------Min Distance between 2 nodes in a binary tree problem------
+    public static int findDistance(node root, int n){
+        if(root == null){
+            return -1;
+        }
+        if(root.data == n){
+            return 0;
+        }
+        int leftDist = findDistance(root.left, n);
+        int rightDist = findDistance(root.right, n);
+
+        if(leftDist != -1 && rightDist == -1){
+            return -1;
+        }else if(leftDist == -1){
+            return rightDist+1;
+        }
+        return rightDist+1;
+    }
+    public static int minDistance(node root, int n1, int n2){
+        node lca = LCA2(root, n1, n2);
+        int dist1 = findDistance(lca, n1);
+        int dist2 = findDistance(lca, n2);
+        return dist1 + dist2;
+    }
+
+    //-----Kth ancestor of a node in a binary tree problem------
+    public static int KthAncestor(node root, int n, int k){
+        if(root == null){
+            return -1;
+        }
+        if(root.data == n){
+            return 0;
+        }
+        int leftDist = KthAncestor(root.left, n, k);
+        int rightDist = KthAncestor(root.right, n, k);
+
+        if(leftDist == -1 && rightDist == -1){
+            return -1;
+        }
+
+        int maxDist = Math.max(leftDist, rightDist);
+        if(maxDist + 1 == k){
+            System.out.println("Kth ancestor is: " + root.data);
+        }
+        return maxDist + 1;
+    }
+
+    //-----Transform to sum tree problem------
+    public static int TransformSum(node root){//O(n)
+        if(root == null){
+            return 0;
+        }
+        int leftchild = TransformSum(root.left);
+        int rightchild = TransformSum(root.right);
+
+        int data = root.data;
+        int newleftchild = root.left == null ? 0 : root.left.data;
+        int newrightchild = root.right == null ? 0 : root.right.data;
+        root.data = newleftchild + leftchild + newrightchild + rightchild;
+        return data;
+    }
+    public static void Preorder(node root){//O(n)
+        if(root == null){
+                // System.out.print("-1 ");
+                return;
+            }
+        System.out.print(root.data + " ");
+        Preorder(root.left);
+        Preorder(root.right);
+    }
+
+    //--------------Practice Questions----------------
+    
     public static void main(String[] args){
         /*
-                1
-               / \
-              2   3
-             / \   \
-            4   5   6
+                 1
+               /  \
+              2    3
+             / \  / \
+            4   57   6
+
+                Expected Output of Transform to Sum Tree:
+                 27
+                /  \
+               11   13
+               / \  / \
+              0   0 0  0
         */
        node root = new node(1);
        root.left = new node(2);
        root.right = new node(3);
        root.left.left = new node(4);
        root.left.right = new node(5);
+       root.right.left = new node(7);
        root.right.right = new node(6);
 
        /*
@@ -247,11 +394,22 @@ public class BinaryTreeB {
             / \
            4   6
        */
-    //   node subRoot = new node(2);
-    //   subRoot.left = new node(4);
-    //   subRoot.right = new node(6);
+        //   node subRoot = new node(2);
+        //   subRoot.left = new node(4);
+        //   subRoot.right = new node(6);
 
-       System.out.println("Top View of Tree: ");
-       TopView(root);
+        //    System.out.println("Top View of Tree: ");
+        //    TopView(root);
+
+        // System.out.println("Kth level of Tree: ");
+        // KthLevel(root, 1, 2);
+
+        // System.out.println("Min Distance between Nodes: " + minDistance(root, 4, 6));
+        //KthAncestor(root, 4, 2);
+
+        // System.out.println("Transform to Sum Tree: ");
+        // TransformSum(root);
+        // Preorder(root);
+
     }
 }
