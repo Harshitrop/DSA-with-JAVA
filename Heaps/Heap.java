4f347525c7816b34dff89a24782d766b93ff10ed
Harshitrop <harshitrathourop@gmail.com>
2026-09-13 18:27:13 +0530

    new Q

--- Heaps/Heap.java
+++ Heaps/Heap.java
@@ -127,89 +127,107 @@ import java.util.*;
 
 public class Heap {
 
-    static class InnerHeap {
+    // static class InnerHeap {
 
-        public static void Heapify(int arr[], int i, int size) {
-            int left = 2 * i + 1;
-            int right = 2 * i + 2;
-            int maxIdx = i;
+    // public static void Heapify(int arr[], int i, int size) {
+    // int left = 2 * i + 1;
+    // int right = 2 * i + 2;
+    // int maxIdx = i;
 
-            if (left < size && arr[left] > arr[maxIdx]) {// for min Heap change ">" to "<"
-                maxIdx = left;
-            }
+    // if (left < size && arr[left] > arr[maxIdx]) {// for min Heap change ">" to
+    // "<"
+    // maxIdx = left;
+    // }
 
-            if (right < size && arr[right] > arr[maxIdx]) {// for min Heap change ">" to "<"
-                maxIdx = right;
-            }
+    // if (right < size && arr[right] > arr[maxIdx]) {// for min Heap change ">" to
+    // "<"
+    // maxIdx = right;
+    // }
 
-            if (maxIdx != i) {
-                // swap
-                int temp = arr[i];
-                arr[i] = arr[maxIdx];
-                arr[maxIdx] = temp;
+    // if (maxIdx != i) {
+    // // swap
+    // int temp = arr[i];
+    // arr[i] = arr[maxIdx];
+    // arr[maxIdx] = temp;
 
-                Heapify(arr, maxIdx, size);
-            }
-        }
+    // Heapify(arr, maxIdx, size);
+    // }
+    // }
 
-        public static void Heapsort(int arr[]) {
-            // step 1 build maxHeap
-            int n = arr.length;
-            for (int i = n / 2; i >= 0; i--) {
-                Heapify(arr, i, n);
-            }
-            // Step 2 push largest at end
-            for (int i = n - 1; i > 0; i--) {
-                // swap
-                int temp = arr[0];
-                arr[0] = arr[i];
-                arr[i] = temp;
+    // public static void Heapsort(int arr[]) {
+    // // step 1 build maxHeap
+    // int n = arr.length;
+    // for (int i = n / 2; i >= 0; i--) {
+    // Heapify(arr, i, n);
+    // }
+    // // Step 2 push largest at end
+    // for (int i = n - 1; i > 0; i--) {
+    // // swap
+    // int temp = arr[0];
+    // arr[0] = arr[i];
+    // arr[i] = temp;
 
-                Heapify(arr, 0, i);
-            }
-        }
+    // Heapify(arr, 0, i);
+    // }
+    // }
 
-        // nearby cars
-        public class point implements comparable<point> {
-            int x;
-            int y;
-            int disSQ;
-            int idx;
+    // // nearby cars
+    // public class point implements comparable<point> {
+    // int x;
+    // int y;
+    // int disSQ;
+    // int idx;
 
-            public point(int x, int y, int disSQ, int idx) {
-                this.x = x;
-                this.y = y;
-                this.disSQ = disSQ;
-                this.idx = idx;
-            }
+    // public point(int x, int y, int disSQ, int idx) {
+    // this.x = x;
+    // this.y = y;
+    // this.disSQ = disSQ;
+    // this.idx = idx;
+    // }
 
-            @Override
-            public int compareTo(point p2) {
-                return this.disSQ - p2.disSQ; // asending order
-            }
-        }
-    }
-
-    public static void main(String args[]) {
-        // int arr[] = { 1,2,4,5,3};
-        // InnerHeap.Heapsort(arr);
-
-        // print
-        // for(int i =0; i<arr.length; i++){
-        // System.out.print(arr[i]+" ");
-        // }
-        // System.out.println();
-
-        // create priority queue
-        PriorityQueue<Point> pq = new PriorityQueue<>();
-        for (int i = 0; i < pts.length; i++) {
-            int distQS = pts[i][0] * pts[i][0] + pts[i][1] * pts[i][1];
-            pq.add(new Point(pts[i][0], pts[i][1], distSQ, i));
-        }
-
-        // nearest k cars
-        for (int i = 0; i < k; i++) {
-            System.out.println("C" = pq.remove().idx);
-        }
-    }
+    // @Override
+    // public int compareTo(point p2) {
+    // return this.disSQ - p2.disSQ; // asending order
+    // }
+    // }
 }
+
+// Connect N ropes
+public static void main(String args[]) {
+    int ropes[] = { 2, 3, 3, 4, 6 };
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    for (int i = 0; i < ropes.length; i++) {
+        pq.add(ropes[i]);
+    }
+    int cost = 0;
+    while (pq.size() > 1) {
+        int first = pq.remove();
+        int second = pq.remove();
+        cost += first + second;
+        pq.add(first + second);
+    }
+    System.out.println("Costs of connecting N Ropes = " + cost);
+}
+
+// public static void main(String args[]) {
+// int arr[] = { 1,2,4,5,3};
+// InnerHeap.Heapsort(arr);
+
+// print
+// for(int i =0; i<arr.length; i++){
+// System.out.print(arr[i]+" ");
+// }
+// System.out.println();
+
+// create priority queue
+// PriorityQueue<Point> pq = new PriorityQueue<>();
+// for (int i = 0; i < pts.length; i++) {
+// int distQS = pts[i][0] * pts[i][0] + pts[i][1] * pts[i][1];
+// pq.add(new Point(pts[i][0], pts[i][1], distSQ, i));
+// }
+
+// // nearest k cars
+// for (int i = 0; i < k; i++) {
+// System.out.println("C" = pq.remove().idx);
+// }
+// }
